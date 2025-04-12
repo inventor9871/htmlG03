6608a475cfefeb64833be075948931c02e8236e9
inventor9871 <118165822+inventor9871@users.noreply.github.com>
2025-04-13 07:13:24 +0800

    change analytics

--- google_analytics.js
+++
@@ -1,8 +0,0 @@
-// <!-- Google tag (gtag.js) -->
-import "https://www.googletagmanager.com/gtag/js?id=G-DL216SYNXN"
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-DL216SYNXN');
